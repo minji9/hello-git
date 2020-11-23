26da44f855528b3a68913f76b5c1e9702cd851b1
minji <haji0408@hansung.ac.kr>
2020-11-23 07:19:34 +0000

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello{
-	public static void main(String[] args){
+    public static void main(String[] args){
 		System.out.println("Hello, world form GIT!");
+    	System.out.println("Changes after First commit!");
+
 	}
 }
+
 
